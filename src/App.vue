69634dfaaae6599942f7ce2cf3e29ac13e5e60bb
Michaël Bourjac <michael.bourjac@gmail.com>
2024-10-31 07:41:05 +0100

    refactor: create AppLayout view

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import HeaderLogo from '@/components/HeaderLogo.vue';
-import NavLink from '@/components/NavLink.vue';
+import AppLayout from '@/layouts/AppLayout/AppLayout.vue';
 </script>
 
 <template>
-  <div class="flex h-dvh flex-col overflow-hidden">
-    <header class="px-5">
-      <div
-        class="flex items-center justify-between border-b border-black py-3.5"
-      >
-        <HeaderLogo />
-        <nav class="flex items-center gap-4 text-2xl leading-none">
-          <NavLink :to="{ name: 'home' }">Home</NavLink>
-          <NavLink :to="{ name: 'projects' }">Projects</NavLink>
-          <NavLink :to="{ name: 'contact' }">Contact</NavLink>
-        </nav>
-      </div>
-    </header>
+  <AppLayout>
     <RouterView />
-  </div>
+  </AppLayout>
 </template>
---
+++ src/layouts/AppLayout/AppLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HeaderLogo from './HeaderLogo.vue';
+import NavLink from './NavLink.vue';
+</script>
+
+<template>
+  <div class="flex h-dvh flex-col overflow-hidden">
+    <header class="px-5">
+      <div
+        class="flex items-center justify-between border-b border-black py-3.5"
+      >
+        <HeaderLogo />
+        <nav class="flex items-center gap-4 text-2xl leading-none">
+          <NavLink :to="{ name: 'home' }">Home</NavLink>
+          <NavLink :to="{ name: 'projects' }">Projects</NavLink>
+          <NavLink :to="{ name: 'contact' }">Contact</NavLink>
+        </nav>
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
